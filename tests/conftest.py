ddee45c49ec5e302aafe2f9fd0a6500a17588f9f
Matt Wade <mwwade@users.noreply.github.com>
2018-10-28 12:34:33 -0700

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,51 @@ import tempfile
 
 import pytest
 
-from flaskr import flaskr
+from flaskr import create_app
+from flaskr.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
 
 @pytest.fixture
-def client():
-    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-    flaskr.app.config['TESTING'] = True
-    client = flaskr.app.test_client()
+def app():
+    db_fd, db_path = tempfile.mkstemp()
 
-    with flaskr.app.app_context():
-        flaskr.init_db()
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
 
-    yield client
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
 
+    yield app
+    
     os.close(db_fd)
-    os.unlink(flaskr.app.config['DATABASE'])
+    os.unlink(db_path)
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
